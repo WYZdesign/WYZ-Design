28c598d6c0592fee9f9837c12d99d75b2b74d442
Torree Harris <torree.marcel@gmail.com>
2026-08-31 20:47:06 -0700

    chore: remove unused useState import from GyroTilt

--- src/components/GyroTilt.tsx
+++ src/components/GyroTilt.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useCallback, useState } from "react";
+import { useRef, useCallback } from "react";
 import { prefersReducedMotion } from "@/lib/utils";
 import { useGyroPermission } from "@/hooks/useGyroPermission";
 
